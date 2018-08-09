1d9066c8854519712a5836ac1f2f72a0d730be55
afrozanishi <afroza1014@gmail.com>
2018-08-09 13:31:21 -0400

    Afroza's website

---
+++ website.js
@@ -0,0 +1,56 @@
+// website.js
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('earlylifeImage');
+    image.addEventListener('click', function (e) {
+        console.log("I just clicked on the earlylifeImage")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/website/earlylife2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (u) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('educationImage');
+    image.addEventListener('click', function (u) {
+        console.log("I just clicked on the educationImage")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/website/education2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (q) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('familyAndPersonalLifeImage');
+    image.addEventListener('click', function (q) {
+        console.log("I just clicked on the familyAndPersonalLifeImage ")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/website/family2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (p) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('interestAndHobbiesImage');
+    image.addEventListener('click', function (p) {
+        console.log("I just clicked on the interestAndHobbiesImage")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/website/wrestling2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (m) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('favoriteThingsImage');
+    image.addEventListener('click', function (m) {
+        console.log("I just clicked on the favoriteThingsImage")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/website/favoritefood2.jpg"
+    
+    })
+})
